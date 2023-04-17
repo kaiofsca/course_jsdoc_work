194749ddddd5054d94709e78e1240db9a3e34d20
Kaio Marques Fonseca <kaio.fonseca@codeplan.df.gov.br>
2023-04-17 11:58:30 -0300

    Type check | Custom Template | Function description | Custom types

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 // @ts-check
 
+{/* ADDING TYPES */}
+
 /**
  * Student Name
  * @type {string}
@@ -19,4 +21,35 @@ const grades = [98, 97.7, 76, 89]
 const todo = {
     id: 1,
     text: 'Hello'
+}
+
+{/* FUNCTION DESCRIPTION */}
+
+/**
+ * Calculate tax
+ * @param {number} amount - total amount
+ * @param {number} tax - tax percentage
+ * @returns {string} - Total with a dollar sign
+ */
+const calculateTax = (amount, tax) => {
+    return `$${amount + tax * amount}`
+}
+
+/**
+ * A Student
+ * @typedef {Object} Student
+ * @property {number} id - Student ID
+ * @property {string} name - Student name
+ * @property {number|string} [age] - Student age (optional)
+ * @property {boolean} isActive - Student is active
+ */
+
+/**
+ * @type {Student}
+ */
+const student = {
+    id: 1,
+    name: 'Kaio M.',
+    age: 19,
+    isActive: true
 }
