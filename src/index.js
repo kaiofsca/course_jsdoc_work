ee52c0cba7e9f9e1a833ded23906ec54cd9cb32b
Kaio Marques Fonseca <kaio.fonseca@codeplan.df.gov.br>
2023-04-17 10:58:25 -0300

    adding a type

---
+++ src/index.js
@@ -0,0 +1,22 @@
+// @ts-check
+
+/**
+ * Student Name
+ * @type {string}
+ */
+const studentName = 'Jonh Doe'
+
+/**
+ * Array of grades
+ * @type {Array<number>}
+ */
+const grades = [98, 97.7, 76, 89]
+
+/**
+ * Todo object
+ * @type {{id: number|string, text: string}}
+ */
+const todo = {
+    id: 1,
+    text: 'Hello'
+}
